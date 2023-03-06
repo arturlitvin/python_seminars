55b43796063fcc07f33b708179408082d40246f2
artur <sro.artur@yandex.ru>
2023-03-06 23:42:30 +0300

    hw6

---
+++ homework6_task32.py
@@ -0,0 +1,17 @@
+# Задача 32: Определить индексы элементов массива (списка), значения которых принадлежат заданному диапазону (т.е. не меньше заданного минимума и не больше заданного максимума)
+
+import random
+
+n = int(input('Введите колличество элементов N: '))
+min_number = int(input('Введите минимум диапазона'))
+max_number = int(input('Введите максимум диапазона'))
+
+my_list = []
+
+for i in range(n):
+    my_list.append(random.randrange(1, 100, 1))
+print(my_list)
+
+for i in range(len(my_list)):
+    if min_number <= my_list[i] <= max_number:
+        print(i)
